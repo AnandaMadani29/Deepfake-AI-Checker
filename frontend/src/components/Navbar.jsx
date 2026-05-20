b02d2252cdb694ee2f14ca8bf0f79749ff269d79
Guntur Listyo <gunturlistyo@gmail.com>
2026-05-20 18:21:14 +0700

    fix bug scroll pada sidebar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,6 +17,25 @@ export default function Navbar({
 }) {
   const [menuOpen, setMenuOpen] = React.useState(false)
 
+  // Prevent body scroll when sidebar is open on mobile
+  React.useEffect(() => {
+    if (isMobile && menuOpen) {
+      document.body.style.overflow = 'hidden'
+      document.body.style.position = 'fixed'
+      document.body.style.width = '100%'
+    } else {
+      document.body.style.overflow = ''
+      document.body.style.position = ''
+      document.body.style.width = ''
+    }
+    
+    return () => {
+      document.body.style.overflow = ''
+      document.body.style.position = ''
+      document.body.style.width = ''
+    }
+  }, [menuOpen, isMobile])
+
   if (isMobile) {
     return (
       <>
